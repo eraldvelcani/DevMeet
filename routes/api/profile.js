06c62695ff2ce85fc123aaa2278ae7e7a8fccaa9
eraldvelcani <eraldvelcani2003@gmail.com>
2025-12-09 16:56:09 +0100

    Added Profile Create and Update functionality

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profiles');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
 //@route GET api/profile/me
 //@desc Get current profile
@@ -19,5 +20,58 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error!');
     }
 });
+
+
+//@route POST api/profile
+//@desc Create / update profile
+//@access Private
+router.post('/', [auth, [check('currentStatus', 'Current status is required!').not().isEmpty()]],
+async (req, res) =>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        company,
+        website,
+        skills,
+        currentStatus,
+        bio,
+        githubUser
+    } = req.body;
+
+    //profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (skills) {
+        profileFields.skills = skills.split(',').map(skill => skill.trim());
+    };
+    if (currentStatus) profileFields.currentStatus = currentStatus;
+    if (bio) profileFields.bio = bio;
+    if (githubUser) profileFields.githubUser = githubUser;
+    
+    try {
+        let profile = await Profile.findOne({ user: req.user.id });
+        if (profile) {
+            //update profile
+            profile = await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true });
+            return res.json(profile)
+        }
+            //create profile
+            profile = new Profile(profileFields);
+            await profile.save();
+            res.json(profile);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error!');
+    }
+
+
+});
+
 
 module.exports = router;
