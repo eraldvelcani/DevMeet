0bad1993ad20dbdacae7f1505a54d66789176cdd
eraldvelcani <eraldvelcani2003@gmail.com>
2025-12-09 12:56:55 +0100

    added user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profiles');
+const User = require('../../models/User');
 
-//@route GET api/profile
-//@desc Test route
-//@access Public
-router.get('/', (req, res) => res.send('Profile route'));
+//@route GET api/profile/me
+//@desc Get current profile
+//@access Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+        if (!profile) {
+            return res.status(400).json({ msg: 'Profile not found.' });
+        }
+        res.json(profile);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error!');
+    }
+});
 
 module.exports = router;
