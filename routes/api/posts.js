d651b21920eb3f66f8f68cc6b4c01ad27dbdd2c1
eraldvelcani <eraldvelcani2003@gmail.com>
2025-12-10 04:18:02 +0100

    added Like and Unlike functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -86,5 +86,41 @@ router.delete('/:id', auth, async(req, res) => {
     }
 });
 
+//@route PUT api/posts/like/:id
+//@desc Like post
+//@access Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) { //if current user has already liked post 
+            return res.status(400).json('Post already liked by same user.');
+        }
+        post.likes.unshift({ user: req.user.id });
+        await post.save();
+        res.json(post.likes);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error!');
+    }
+});
+
+//@route PUT api/posts/unlike/:id
+//@desc Retract your like
+//@access Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) { //length of 0 means not yet liked
+            return res.status(400).json('Post not liked yet.');
+        }
+        const toBeRemoved = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+        post.likes.splice(toBeRemoved, 1);
+        await post.save();
+        res.json(post.likes);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error!');
+    }
+});
 
 module.exports = router;
